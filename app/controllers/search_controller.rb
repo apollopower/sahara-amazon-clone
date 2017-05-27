7c69df87d9f6b6b021fe733253f22b83c640224d
Apollo <erthaljonas@gmail.com>
2017-05-27 18:36:01 -0400

    Ebay Api now working WITHOUT SEARCH BOX

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,5 @@
 class SearchController < ApplicationController
   def index
-    response = HTTParty.get("https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=hewy+lewis+and+the+news")
-
-    dom = NOKOGIRI::HTML(response.body)
+    @search = Search.new('laptops')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527161436) do
+ActiveRecord::Schema.define(version: 20170527211923) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20170527161436) do
     t.integer "user_id"
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
