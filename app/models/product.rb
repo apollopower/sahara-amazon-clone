8de8b37a76df562dc60b1aae7a58ec9547306ff3
Apollo <erthaljonas@gmail.com>
2017-05-27 11:25:56 -0400

    Created relation between Users and Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true, greater_than: 0}
 
+  belongs_to :user
   has_many :reviews
 
   def formatted_price
