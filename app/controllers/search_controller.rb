5619bd4b4507c409915486a7f1e8af29c8ed35b9
Misael Bejarano <vonleny@gmail.com>
2017-05-28 10:54:45 -0400

    Fixing errors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,5 @@
 class SearchController < ApplicationController
   def index
-<<<<<<< HEAD
-    response = HTTParty.get("https://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=hewy+lewis+and+the+news")
-
-    dom = NOKOGIRI::HTML(response.body)
-=======
     @search = Search.new('laptops')
->>>>>>> 7c69df87d9f6b6b021fe733253f22b83c640224d
   end
 end
