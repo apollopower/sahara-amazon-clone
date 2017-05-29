59bfe20d30d1bbcc3fc0cbf909a8679df3f08434
Apollo <erthaljonas@gmail.com>
2017-05-29 18:06:25 -0400

    Added buy button to Search view

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,27 @@
+// $(document).on('click', '.a-button', function(){
+//   console.log("Hello");
+// })
+
+// function thankYou() {
+//   console.log("Hey there");
+//   var x = document.getElementById('thank-you');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// }
+
+$(function(){
+  $('.thank-you').hide();
+})
+
+// $(document).on('click', '.a-button', function(){
+//   var x = $(document).find('.thank-you')[0].text();
+//   console.log(x);
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// })
